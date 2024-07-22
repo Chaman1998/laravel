22e48bc998e98f8e825148f407f8fd45e3f721ad
chaman1998 <chamansarkar11@gmail.com>
2024-07-22 11:33:56 +0530

    learn Components

---
+++ blog/resources/views/components/massage-banner.blade.php
@@ -0,0 +1,9 @@
+<div>
+    <h4>This fetch from massage-banner Components</h4>
+
+    <span class="success">User Login Suggessfully</span>
+    {{-- <span class="success">{{$msg}}</span> --}}
+
+    <span class="{{$class}}">{{$msg}}</span>
+    <!-- Very little is needed to make a happy life. - Marcus Aurelius -->
+</div>
--- blog/resources/views/welcome.blade.php
+++ blog/resources/views/welcome.blade.php
@@ -1,2 +1,26 @@
 <h1>Hello laravel</h1>
 <a href="home">Home</a>
+
+<br/>
+<x-massage-banner msg="User login massage send Successfully" class="success"/>
+
+<x-massage-banner msg="Password not correct." class="error"></x-massage-banner>
+
+<style>
+    .success{
+        background: lightgreen;
+        color: green;
+        padding: 5px 10px;
+        border-radius: 5px;
+        display: inline-block;
+        margin: 10px;
+    }
+    .error{
+        background: red;
+        color: white;
+        padding: 5px 10px;
+        border-radius: 5px;
+        display: inline-block;
+        margin: 10px;
+    }
+</style>
