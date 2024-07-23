b7f8a1e1f1e8c1f79df9376619956c9b860b3b4f
chaman1998 <chamansarkar11@gmail.com>
2024-07-23 22:35:33 +0530

    Form validation

--- blog/app/Http/Controllers/FormController.php
+++ blog/app/Http/Controllers/FormController.php
@@ -8,6 +8,25 @@ class FormController extends Controller
 {
     //
     function addUser(Request $request){
+
+        $request->validate([
+            'username'=>'required | min:3 | uppercase',
+            'email'=>'required | email',
+            'city'=>'required | min:4',
+        ],[
+            'username.required'=>'username can not be empty',
+            'email.required'=>'Email can not be empty',
+            'email.email'=>'Must be use proper Email ID',
+            'city.required'=>'City can not be empty',
+        ]);
+
+        // php artisan lang:publish
+        // for customize validation massage
+
+        //for make own customized rule
+        //php artisan make:rule Uppercase
+
+        
         echo $request->username;
         echo "<br/>";
         echo $request->email;
--- blog/resources/views/user-form.blade.php
+++ blog/resources/views/user-form.blade.php
@@ -2,7 +2,7 @@
     input {
         border: black 1px solid;
         height: 35px;
-        width: 200px;
+        width: 300px;
         border-radius: 5px;
         color: black;
     }
@@ -10,38 +10,72 @@
     .input-wrapper {
         margin: 5px;
         padding: 5px;
+        display: grid;
+        justify-content: center;
+        align-items: center;
     }
 
     button {
         border: green 2px solid;
         height: 35px;
-        width: 200px;
+        width: 300px;
         border-radius: 5px;
         color: black;
         background-color: #fff;
         cursor: pointer;
     }
-    h3{
+
+    h3 {
         text-align: center;
     }
+
     .center {
         display: grid;
         justify-content: center;
         align-items: center;
     }
+    span{
+        margin-top: 2px;
+        color: red;
+    }
+    .input-error{
+        border: red 1px solid;
+        color: red;
+    }
 </style>
 <div class="center">
     <h3>Add New User</h3>
+
+    {{-- {{
+        print_r($errors)
+    }} --}}
+    {{-- @if ($errors->any())
+        @foreach ($errors->all() as $error)
+            <div>
+                {{ $error }}
+            </div>
+        @endforeach
+    @endif --}}
+
     <form action="adduser" method="post">
         @csrf
         <div class="input-wrapper">
-            <input type="text" placeholder="Enter user name" name="username">
+            <input type="text" placeholder="Enter user name" name="username" value="{{old('username')}}"
+            class="{{$errors->first('username') ? 'input-error' : ''}}"
+            >
+            <span>@error('username'){{$message}}@enderror</span>
         </div>
         <div class="input-wrapper">
-            <input type="text" placeholder="Enter user Email" name="email">
+            <input type="text" placeholder="Enter user Email" name="email" value="{{old('email')}}"
+            class="{{$errors->first('email') ? 'input-error' : ''}}"
+            >
+            <span>@error('email'){{$message}}@enderror</span>
         </div>
         <div class="input-wrapper">
-            <input type="text" placeholder="Enter user City" name="city">
+            <input type="text" placeholder="Enter user City" name="city" value="{{old('city')}}"
+            class="{{$errors->first('city') ? 'input-error' : ''}}"
+            >
+            <span>@error('city'){{$message}}@enderror</span>
         </div>
         <div class="input-wrapper">
             <button>Add New User</button>
