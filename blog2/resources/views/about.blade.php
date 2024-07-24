da94884f210c1a059c06ffe9279861cb7c1768cc
chaman1998 <chamansarkar11@gmail.com>
2024-07-24 10:58:11 +0530

    Url create

---
+++ blog2/resources/views/about.blade.php
@@ -0,0 +1,5 @@
+<div>
+    <h3>About us</h3>
+    <h3>{{URL::previous()}}</h3>
+    <!-- Happiness is not something readymade. It comes from your own actions. - Dalai Lama -->
+</div>
--- blog2/resources/views/home.blade.php
+++ blog2/resources/views/home.blade.php
@@ -1,10 +1,8 @@
 <div>
-    <h3>Welcome Page</h3>
-   
-    <a href="{{URL::to('home')}}">Home Page</a>
+    <h3>Home page</h3>
+    <a href="about">About Page</a>
+    <p>Another way to create a tag.</p>
     <a href="{{URL::to('about')}}">About Page</a>
-    <a href="{{URL::to('about',['name'])}}">About Name</a>
-
     <h5>{{ URL::current() }}</h5>
     <h5>{{ URL()->current() }}</h5>
 
