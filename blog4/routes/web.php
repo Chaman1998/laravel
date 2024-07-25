22180917506bb4cfd63a91d1728dd166822e95c5
chaman1998 <chamansarkar11@gmail.com>
2024-07-25 19:44:11 +0530

    middleware check

---
+++ blog4/routes/web.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::view('home','home')->middleware('check1');
+Route::view('about','about');
