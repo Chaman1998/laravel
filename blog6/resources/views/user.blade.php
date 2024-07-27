c08e80c474eda7181f5f2a6f2b51292b5c57a813
chaman1998 <chamansarkar11@gmail.com>
2024-07-27 10:27:28 +0530

    fetch data from API

---
+++ blog6/resources/views/user.blade.php
@@ -0,0 +1,9 @@
+<div>
+    <h3>User page</h3>
+    {{-- {{print_r($data)}} --}}
+    <ul>
+        <li><p><span>Name</span>:{{$data->name}}</p></li>
+        <li><p><span>User</span>:{{$data->username}}</p></li>
+        <li><p><span>email</span>:{{$data->email}}</p></li>
+    </ul>
+</div>
