904df6f8100a525b21946d0a0bde1249ed02bf95
chaman1998 <chamansarkar11@gmail.com>
2024-08-06 20:10:48 +0530

    Create, Read, Update, Delete, And Search data with Laravel API

---
+++ blog14/routes/api.php
@@ -0,0 +1,19 @@
+<?php
+
+use App\Http\Controllers\StudentController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+// Route::get("test",function(){
+//     return ["name"=>"Chaman","age"=>"24"];
+// });
+
+Route::get("students",[StudentController::class,'list']);
+Route::post("add-student",[StudentController::class,'addStudent']);
+Route::put("update-student",[StudentController::class,'updateStudent']);
+Route::delete("delete-student/{id}",[StudentController::class,'deleteStudent']);
+Route::get("search-student/{name}",[StudentController::class,'searchStudent']);
